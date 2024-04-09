89f598b42cc85f0f5746af5b097cfa4215903c4c
Jiří Mírovský <jmirovsky@github.com>
2024-04-09 12:40:32 +0000

    Fixed missing spaces between sentences
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/ponk/projects/maskit@184 9b7822f3-4786-4c31-9c96-ea234496b5ad

--- anonymize_stdin_start.sh
+++ anonymize_stdin_start.sh
@@ -1,7 +1,7 @@
 
 echo "Anonymizing input text" >err
 
-echo "Paní Nováková pracuje ve firmě Česká plynárenská, s.r.o." |\
-./system/maskit.pl --stdin --diff --randomize --named-entities 2 --output-format txt 2>>err
+echo -e "Paní Nováková pracuje ve firmě Česká plynárenská, s.r.o. Pan Novák zůstává v souvislosti s prací v domě.\nJsou manželé." |\
+./system/maskit.pl --stdin --diff --randomize --named-entities 2 --output-format txt --store-format conllu 2>>err
 
 echo "Anonymization finished." >>err
--- system/maskit.pl
+++ system/maskit.pl
@@ -21,7 +21,7 @@ binmode STDERR, ':encoding(UTF-8)';
 
 my $start_time = [gettimeofday];
 
-my $VER = '0.54 20240307'; # version of the program
+my $VER = '0.54 20240409'; # version of the program
 
 my @features = ('first names',
                 'surnames (male and female tied)',
@@ -1873,6 +1873,8 @@ END_OUTPUT_HEAD
 
 =cut
 
+  my $space_before = ''; # for storing info about SpaceAfter until the next token is printed
+
   foreach $root (@trees) {
 
 #=item 
@@ -1929,7 +1931,6 @@ END_OUTPUT_HEAD
 
     # PRINT THE SENTENCE TOKEN BY TOKEN
     my @nodes = sort {attr($a, 'ord') <=> attr($b, 'ord')} descendants($root);
-    my $space_before = '';
 
     foreach my $node (@nodes) {
 
@@ -2055,9 +2056,9 @@ END_OUTPUT_HEAD
 
         $output .= "$space_before$span_start$form$span_end$info_span";
 
-        $space_before = ($SpaceAfter eq 'No') ? '' : ' '; # this way there will not be space after the last token of the sentence
-
-        # $output .= "($SpacesAfter)"; # debug info
+        $space_before = ($SpaceAfter eq 'No' or $SpacesAfter) ? '' : ' '; # store info about a space until the next token is about to be printed
+        
+        # $output .= "\ndebug info: SpaceAfter='$SpacesAfter', space_before = '$space_before'\n";
         # handle extra spaces and newlines in SpaceAfter
         if ($SpacesAfter =~ /^(\\s|\\r|\\n)+$/) { # SpacesAfter informs that there were newlines or extra spaces in the original text here
           if ($format eq 'html') {
@@ -2076,8 +2077,8 @@ END_OUTPUT_HEAD
             $SpacesAfter =~ s/\\n/\n/g;
             $SpacesAfter =~ s/\\s/ /g;
           }
-          $output .= $SpacesAfter;          
         }
+        $output .= $SpacesAfter;
         
       }
       elsif ($format eq 'conllu') {
