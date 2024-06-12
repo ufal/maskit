5f90cc49a8abb39f84bb8c0195b8b0d4309c587a
Jiří Mírovský <jmirovsky@github.com>
2024-06-12 14:59:26 +0000

    Error by SC
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/ponk/projects/maskit@224 9b7822f3-4786-4c31-9c96-ea234496b5ad

---
+++ anonymize_errors_by_TN_start.sh
@@ -0,0 +1,16 @@
+
+echo "Anonymizing errors_by_TN.txt" >err
+
+for FILE in data/errors_by_TN.txt; do
+
+  echo >>err
+  echo "Anonymizing input text $FILE" >>err
+  echo >>err
+
+  cat $FILE |\
+  ./system/maskit.pl --input-file $FILE --input-format txt --diff --named-entities 2 --output-format txt --store-format conllu 2>>err
+
+done
+
+echo
+echo "Anonymization of the data finished." >>err
